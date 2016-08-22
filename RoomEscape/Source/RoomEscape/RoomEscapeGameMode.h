a4688b9f7635275ecafb1d52df29a288013d0d70
NORTOKA\Julia <pixys7ix@gmail.com>
2016-08-21 23:53:35 -0700

    Initial Project Commit

---
+++ RoomEscape/Source/RoomEscape/RoomEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RoomEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMESCAPE_API ARoomEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
